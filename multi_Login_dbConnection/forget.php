a0efbb974768f4d2f24d34ee324e273c1f36a2b7
SHAHRIAR UTCHAS <123489314+Shahriar-Utchas@users.noreply.github.com>
2024-03-18 17:35:59 +0600

    Add files via upload

---
+++ multi_Login_dbConnection/forget.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Forget password</title>
+</head>
+<body>
+    <h3>Change Your Password</h3>
+    <?php 
+     include("database.php");
+    ?>
+    <form method ="POST">
+        Enter your E-mail: <br>
+        <input type="Email" name="mail"> <br> <br>   
+        <?php 
+            if(isset($_POST["check"])){
+                session_start();
+                $mail = $_POST['mail'];
+                $_SESSION['mail']=$mail;
+                $sql="select * from user where Email = '$mail'";
+                try{
+                    $result=mysqli_query($conn,$sql);
+                    if($result-> num_rows>0) {
+                        ?>  
+                        Enter your new password: <br>
+                        <input type="password" name="pass"> <br> 
+                        Re-type your new password: <br>
+                        <input type="password" name="Rpass"> <br> <br>
+                        <input type="submit" name ="chng" value="Change Password">
+                        <?php   
+                                         
+                    }
+                    else{
+                        echo "Couldn't found user<br>";
+                    }
+                }
+                catch(mysqli_sql_exception){
+                    echo "Couldn't found user<br>";
+                }
+                ?>
+                
+                <?php
+            }
+        ?>
+        <input type="submit" name="check">
+        <input type="submit" name="login" value ="login">
+    </form>
+            <?php 
+            if(isset($_POST["chng"])){
+                if(!empty($_POST['pass']) && !empty($_POST['Rpass'])){
+                    $npass= $_POST['pass'];
+                    $Rnpass= $_POST['Rpass'];
+                    if($npass == $Rnpass){
+                        session_start();
+                        $mail=$_SESSION['mail'];
+                        $passSql="UPDATE user SET pass ='$npass' WHERE Email ='$mail'";
+                        mysqli_query($conn,$passSql);
+                        echo"Password Changed!";
+                    }else{
+                        echo"password mismatched";
+                    }
+                }else{
+                    echo "Need Fill up the Password Box";
+                } 
+            }
+            if(isset($_POST["login"])){
+                header("location:login.php");
+            }               
+            ?>
+   
+</body>
+</html>
